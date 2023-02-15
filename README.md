cc1b09ce1243397f0a5e03807248875836c124f9
phil1436 <philxsb@gmail.com>
2023-02-15 21:58:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# PhilOnline
+
+[WebHost](https://www.000webhost.com/members/website/list)
+
+[WebSite](https://phil-online.000webhostapp.com/)
---
+++ main.js
@@ -0,0 +1,58 @@
+let slideIndex = 1;
+
+let interval = setInterval(nextSlideIntervall, 5000);
+
+function init() {
+   showSlides(1);
+}
+
+// Next/previous controls
+function plusSlides(n) {
+   slideIndex += n;
+   showSlides(slideIndex);
+}
+
+function nextSlideIntervall() {
+   slideIndex += 1;
+   showSlides(slideIndex);
+}
+function nextSlide() {
+   clearInterval(interval);
+   slideIndex += 1;
+   showSlides(slideIndex);
+}
+function previousSlide() {
+   clearInterval(interval);
+   slideIndex -= 1;
+   showSlides(slideIndex);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+   clearInterval(interval);
+   slideIndex = n;
+   showSlides(slideIndex);
+}
+
+function showSlides(n) {
+   console.log(n);
+   let i;
+   let slides = document.getElementsByClassName('mySlides');
+   let dots = document.getElementsByClassName('dot');
+
+   if (n > slides.length) {
+      slideIndex = 1;
+   }
+   if (n < 1) {
+      slideIndex = slides.length;
+   }
+
+   for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+   }
+   for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+   }
+   slides[slideIndex - 1].style.display = 'block';
+   dots[slideIndex - 1].className += ' active';
+}
