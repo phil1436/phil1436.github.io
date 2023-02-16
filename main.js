a7de49f61773b8ab7befe7fd577810532d1dbcdb
phil1436 <philxsb@gmail.com>
2023-02-16 18:36:28 +0100

    wave added

--- main.js
+++ main.js
@@ -3,56 +3,69 @@ let slideIndex = 1;
 let interval = setInterval(nextSlideIntervall, 5000);
 
 function init() {
-   showSlides(1);
+    showSlides(1);
 }
 
 // Next/previous controls
 function plusSlides(n) {
-   slideIndex += n;
-   showSlides(slideIndex);
+    slideIndex += n;
+    showSlides(slideIndex);
 }
 
 function nextSlideIntervall() {
-   slideIndex += 1;
-   showSlides(slideIndex);
+    slideIndex += 1;
+    showSlides(slideIndex);
 }
 function nextSlide() {
-   clearInterval(interval);
-   slideIndex += 1;
-   showSlides(slideIndex);
+    clearInterval(interval);
+    slideIndex += 1;
+    showSlides(slideIndex);
 }
 function previousSlide() {
-   clearInterval(interval);
-   slideIndex -= 1;
-   showSlides(slideIndex);
+    clearInterval(interval);
+    slideIndex -= 1;
+    showSlides(slideIndex);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-   clearInterval(interval);
-   slideIndex = n;
-   showSlides(slideIndex);
+    clearInterval(interval);
+    slideIndex = n;
+    showSlides(slideIndex);
 }
 
 function showSlides(n) {
-   console.log(n);
-   let i;
-   let slides = document.getElementsByClassName('mySlides');
-   let dots = document.getElementsByClassName('dot');
+    console.log(n);
+    let i;
+    let slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('dot');
 
-   if (n > slides.length) {
-      slideIndex = 1;
-   }
-   if (n < 1) {
-      slideIndex = slides.length;
-   }
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
 
-   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-   }
-   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' active', '');
-   }
-   slides[slideIndex - 1].style.display = 'block';
-   dots[slideIndex - 1].className += ' active';
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
 }
+
+const wrapper = document.getElementById('bubble-wrapper');
+const animateBubble = (x) => {
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    bubble.style.left = x + 'px';
+    wrapper.appendChild(bubble);
+    setTimeout(() => wrapper.removeChild(bubble), 2000);
+};
+
+window.onmousemove = (e) => {
+    animateBubble(e.clientX);
+};
