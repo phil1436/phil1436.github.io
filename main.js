b39df1d673966a486dbf1f5caae6de7497eb85be
phil1436 <philxsb@gmail.com>
2023-02-17 15:49:28 +0100

    v2

--- main.js
+++ main.js
@@ -1,58 +1,92 @@
-let slideIndex = 1;
+let right = document.getElementById('right');
+let left = document.getElementById('left');
+const windowwidthCenter = window.innerWidth / 2;
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    console.log(value);
 
-let interval = setInterval(nextSlideIntervall, 5000);
-
-function init() {
-    showSlides(1);
-}
-
-// Next/previous controls
-function plusSlides(n) {
-    slideIndex += n;
-    showSlides(slideIndex);
-}
-
-function nextSlideIntervall() {
-    slideIndex += 1;
-    showSlides(slideIndex);
-}
-function nextSlide() {
-    clearInterval(interval);
-    slideIndex += 1;
-    showSlides(slideIndex);
-}
-function previousSlide() {
-    clearInterval(interval);
-    slideIndex -= 1;
-    showSlides(slideIndex);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    clearInterval(interval);
-    slideIndex = n;
-    showSlides(slideIndex);
-}
-
-function showSlides(n) {
-    console.log(n);
-    let i;
-    let slides = document.getElementsByClassName('mySlides');
-    let dots = document.getElementsByClassName('dot');
-
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
+    //about
+    let about = document.getElementById('about');
+    if (value > 1200 && value < 2200) {
+        about.style.position = 'fixed';
+        about.style.top = '60px';
+    } else if (value >= 2200) {
+        about.style.position = 'relative';
+        about.style.top = '2000px';
+    } else if (value <= 1200) {
+        about.style.position = 'relative';
+        about.style.top = '1000px';
     }
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
+    // contact
+    let contact = document.getElementById('contact');
+    if (value > 3400 && value < 4400) {
+        contact.style.position = 'fixed';
+        contact.style.top = '100px';
+    } else if (value >= 4400) {
+        contact.style.position = 'relative';
+        contact.style.top = '3900px';
+    } else if (value <= 3400) {
+        contact.style.position = 'relative';
+        contact.style.top = '3000px';
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(' active', '');
+    //repos
+    let downRepo = 6000;
+    showRepo(value, 'ownobjectscriptextension', downRepo, downRepo + 2000);
+    showRepo(value, 'owngitextension', downRepo + 2000, downRepo + 4000);
+    showRepo(value, 'ownvscodeextension', downRepo + 4000, downRepo + 6000);
+    showRepo(value, 'mtconnect-objectscript', downRepo + 6000, downRepo + 8000);
+    showRepo(value, 'node-red-contrib-iris', downRepo + 8000, downRepo + 10000);
+
+    let upperRepo = 17000;
+
+    let repos = document.getElementById('repos');
+    if (value > 5800 && value < upperRepo) {
+        repos.style.position = 'fixed';
+        repos.style.top = '100px';
+    } else if (value >= upperRepo) {
+        repos.style.position = 'relative';
+        repos.style.top = upperRepo - 700 + 'px';
+    } else if (value <= 5800) {
+        repos.style.position = 'relative';
+        repos.style.top = '5000px';
     }
-    slides[slideIndex - 1].style.display = 'block';
-    dots[slideIndex - 1].className += ' active';
+
+    if (value > upperRepo + 1000) {
+        if ((value - (upperRepo + 1000)) * 0.5 > window.innerWidth / 2 - 150)
+            return;
+        left.style.left = (value - (upperRepo + 1000)) * 0.5 + 'px';
+
+        right.style.left =
+            window.innerWidth - 300 - (value - (upperRepo + 1000)) * 0.5 + 'px';
+        return;
+    }
+
+    if (windowwidthCenter - value * 0.5 - 150 > 0) {
+        right.style.left = window.innerWidth / 2 + value * 0.5 - 150 + 'px';
+        left.style.left = window.innerWidth / 2 - value * 0.5 - 150 + 'px';
+    } else {
+        right.style.left = window.innerWidth - 300 + 'px';
+        left.style.left = 0 + 'px';
+    }
+});
+
+function showRepo(value, id, from, to) {
+    let repo = document.getElementById(id);
+    /* if (value > from && value < to) {
+        repo.style.display = 'flex';
+    } else {
+        repo.style.display = 'none';
+    } */
+    if (value > to) {
+        repo.style.left = '-3000px';
+        return;
+    }
+    if (value < from) {
+        repo.style.left = '-3000px';
+        return;
+    }
+
+    if (value < from + 100) repo.style.left = value - from + 'px';
+    if (value > from + 800) repo.style.left = value - (from + 700) + 'px';
 }
